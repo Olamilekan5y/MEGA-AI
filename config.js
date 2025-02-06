cf57e6c88065351153898b27b181ade4ab96a84a
Olamilekan5y <168233117+Olamilekan5y@users.noreply.github.com>
2025-02-06 17:25:42 -0600

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '447874458252' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'DcQwyarQ#FzOBmedY4G7J2GVa90mjQ3WqFE8ymxIQ-iyjOEoBcK4' ;  // put your session id here
 
 global.mods = []
 global.prems = []
